b5820bd9b656c9879ce744419e69e3adb8b8c61d
Frederic <locaplux@outlook.com>
2026-07-05 21:36:49 +0200

    Add sendOrderEmails aggregator

---
+++ lib/emails/order/senOrderEmails.ts
@@ -0,0 +1,7 @@
+export { default as sendOrderCancelledEmail } from "./orderCancelled";
+export { default as sendOrderConfirmationEmail } from "./orderConfirmation";
+export { default as sendOrderDeliveredEmail } from "./orderDelivered";
+export { default as sendOrderNewEmail } from "./orderNew";
+export { default as sendOrderReceiptEmail } from "./orderReceipt";
+export { default as sendOrderShippedEmail } from "./orderShipped";
+export { default as sendOrderStatusUpdateEmail } from "./orderStatusUpdate";
