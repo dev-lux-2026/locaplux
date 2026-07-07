932ab6545b9e6af2de04921481b90f4392b78800
Frederic <locaplux@outlook.com>
2026-07-07 17:15:19 +0200

    fix order email exports

--- lib/emails/order/sendOrderEmails.ts
+++ lib/emails/order/sendOrderEmails.ts
@@ -1,7 +1,7 @@
-export { default as sendOrderCancelledEmail } from "./orderCancelled";
-export { default as sendOrderConfirmationEmail } from "./orderConfirmation";
-export { default as sendOrderDeliveredEmail } from "./orderDelivered";
-export { default as sendOrderNewEmail } from "./orderNew";
-export { default as sendOrderReceiptEmail } from "./orderReceipt";
-export { default as sendOrderShippedEmail } from "./orderShipped";
-export { default as sendOrderStatusUpdateEmail } from "./orderStatusUpdate";
+export { emailOrderCancelled as sendOrderCancelledEmail } from "./orderCancelled";
+export { emailOrderConfirmation as sendOrderConfirmationEmail } from "./orderConfirmation";
+export { emailOrderDelivered as sendOrderDeliveredEmail } from "./orderDelivered";
+export { emailOrderNew as sendOrderNewEmail } from "./orderNew";
+export { emailOrderReceipt as sendOrderReceiptEmail } from "./orderReceipt";
+export { emailOrderShipped as sendOrderShippedEmail } from "./orderShipped";
+export { emailOrderStatusUpdate as sendOrderStatusUpdateEmail } from "./orderStatusUpdate";
