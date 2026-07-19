b1e69b11253770eb1bebfa523beb029519a76611
Frederic <locaplux@outlook.com>
2026-07-19 13:14:30 +0200

    Fix typing in delivery route

--- app/api/account/orders/[id]/delivery/route.ts
+++ app/api/account/orders/[id]/delivery/route.ts
@@ -1,7 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(req, { params }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
   const order = await prisma.order.findUnique({
