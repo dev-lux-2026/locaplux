dba39e77498f576ee23bcc9ae23ac720fbdfd9b8
Frederic <locaplux@outlook.com>
2026-07-22 21:51:11 +0200

    Fix list route: add Request type

--- app/api/admin/categories/list/route.ts
+++ app/api/admin/categories/list/route.ts
@@ -1,7 +1,10 @@
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  const categories = await prisma.category.findMany();
+export async function GET(req: Request) {
+  const categories = await prisma.category.findMany({
+    orderBy: { name: "asc" },
+  });
+
   return NextResponse.json(categories);
 }
