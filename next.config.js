b8bfdb26c03d21bf953e8f66aeb765fec7eced66
Frederic <locaplux@outlook.com>
2026-07-26 16:12:11 +0200

    Disable ESLint during Vercel build

--- next.config.js
+++ next.config.js
@@ -5,3 +5,11 @@ const withIntl = withNextIntl('./src/i18n/request.ts');
 export default withIntl({
   reactStrictMode: true
 });
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
