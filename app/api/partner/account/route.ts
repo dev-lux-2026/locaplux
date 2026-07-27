9e25ec09f25f6d37f8afb1f72adb62979febc903
Frederic <locaplux@outlook.com>
2026-07-27 10:47:17 +0200

    Fix TS strict: type req parameter

--- app/api/partner/account/route.ts
+++ app/api/partner/account/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 
-export async function GET(req) {
+export async function GET(req: Request) {
   const session = await getServerSession();
 
   if (!session?.user?.email) {
