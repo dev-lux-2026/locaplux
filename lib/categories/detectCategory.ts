71177a9b3c5b32210f1214d8f4aa0ed2cc2d92d9
Frederic <locaplux@outlook.com>
2026-08-22 12:14:21 +0200

    Fix TS: cast key as keyof categoryMapping

--- lib/categories/detectCategory.ts
+++ lib/categories/detectCategory.ts
@@ -4,7 +4,7 @@ export function detectCategoryFromTags(tags: string[]) {
   const lowerTags = tags.map((t) => t.toLowerCase());
 
   for (const key in categoryMapping) {
-    const entry = categoryMapping[key];
+    const entry = categoryMapping[key as keyof typeof categoryMapping]; // FIX TS STRICT
 
     const match = entry.tags.some((tag) => lowerTags.includes(tag));
     if (match) {
